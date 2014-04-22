e85ddfb4ff4b6172cbb837b485ef7c6740682ffb
Giovanni P <pignoni.g@gmail.com>
2014-04-22 17:29:28 +0200

    Revert a54e3b2..98d6913
    This rolls back to commit a54e3b2bf5b3b9f9275637ab320b887840d204b3.

--- calc/bezIntersection.py
+++ calc/bezIntersection.py
@@ -1,33 +1,33 @@
 ########################
 # Beziers intersection #
 ########################
-from math import *
+
 class bezierIntersection:
     def __init__(self):
         pass
     
 
-    def calc_bez_length (self,x1,y1,x2,y2,x3,y3,x4,y4,Delta_x_imput_max):
-        """(x1,y1,x2,y2,x3,y3,x4,y4,Delta_x_imput_max)
+    def calc_bez_length (xI,yI,xII,yII,xIII,yIII,xIV,yIV,Delta_x_imput_max):
+        """(xI,yI,xII,yII,xIII,yIII,xIV,yIV,Delta_x_imput_max)
         calculate the length of a bezier, and the t fraction needed to calulate the imput x precision
         example  for Delta_x_imput_max = 0.001   and a bezier with a length = 100 we will have a Delta_t_bez_cal = 0.00001
         return a tuple (x,y)"""
     
         Delta_t_bez_cal=0.1
     
-        x_strt=x1
-        y_strt=y1
+        x_strt=xI
+        y_strt=yI
     
         l_bex=0
     
         t_bez_cal=0
 
-        cx = 3*(x2 - x1)
-        bx = 3*(x3 - x2) - cx
-        ax = x4 - x1 - cx - bx
-        cy = 3*(y2 - y1)
-        by = 3*(y3 - y2) - cy
-        ay = y4 - y1 - cy - by
+        cx = 3*(xII - xI)
+        bx = 3*(xIII - xII) - cx
+        ax = xIV - xI - cx - bx
+        cy = 3*(yII - yI)
+        by = 3*(yIII - yII) - cy
+        ay = yIV - yI - cy - by
 
         while True:
             if t_bez_cal >= 1:
@@ -38,8 +38,8 @@ class bezierIntersection:
                 t_bez_cal3=t_bez_cal*t_bez_cal*t_bez_cal
                 t_bez_cal2=t_bez_cal*t_bez_cal
             
-                x_bez_cal = ax*(t_bez_cal3) + bx*(t_bez_cal2) + cx*t_bez_cal + x1
-                y_bez_cal = ay*(t_bez_cal3) + by*(t_bez_cal2) + cy*t_bez_cal + y1
+                x_bez_cal = ax*(t_bez_cal3) + bx*(t_bez_cal2) + cx*t_bez_cal + xI
+                y_bez_cal = ay*(t_bez_cal3) + by*(t_bez_cal2) + cy*t_bez_cal + yI
 
                 t_bez_cal=t_bez_cal+Delta_t_bez_cal
             
@@ -55,30 +55,28 @@ class bezierIntersection:
         return l_bex, Delta_t_bez_cal
     
     
-    def calc_bez_xy (self,X1,x1,y1,x2,y2,x3,y3,x4,y4,Delta_x_imput_max,Delta_t_bez_cal):
+    def calc_bez_xy (XI,xI,yI,xII,yII,xIII,yIII,xIV,yIV,Delta_x_imput_max,Delta_t_bez_cal):
         
-        """(X1,x1,y1,x2,y2,x3,y3,x4,y4,Delta_x_imput_max,Delta_t_bez_cal)
+        """(XI,xI,yI,xII,yII,xIII,yIII,xIV,yIV,Delta_x_imput_max,Delta_t_bez_cal)
         calculate y coordinate on a bezier curve given a x value
         Delta_x_imput_max is the precision required es (0.001) 
         Delta_t_bez_cal is the scan precision es (0.001) 
         if Delta_t_bez_cal is == a sutable number will be calculated using calc_bez_length """
         
         if Delta_t_bez_cal== None:
-            
-            
-            calc_bez= self.calc_bez_length (x1,y1,x2,y2,x3,y3,x4,y4,Delta_x_imput_max)
+            calc_bez= calc_bez_length (self,xI,yI,xII,yII,xIII,yIII,xIV,yIV,Delta_x_imput_max)
             Delta_t_bez_cal=calc_bez[1]
-        x_imput=X1        
+        x_imput=XI        
         t_bez_cal=0
     
-        cx = 3*(x2 - x1)
-        bx = 3*(x3 - x2) - cx
-        ax = x4 - x1 - cx - bx
-        cy = 3*(y2 - y1)
-        by = 3*(y3 - y2) - cy
-        ay = y4 - y1 - cy - by
+        cx = 3*(xII - xI)
+        bx = 3*(xIII - xII) - cx
+        ax = xIV - xI - cx - bx
+        cy = 3*(yII - yI)
+        by = 3*(yIII - yII) - cy
+        ay = yIV - yI - cy - by
     
-        Delta_x_imput_eff=float("inf")
+        Delta_x_imput_eff=1000000000
 
         while True:
             if t_bez_cal>1 or Delta_x_imput_eff <= Delta_x_imput_max:
@@ -89,88 +87,40 @@ class bezierIntersection:
             t_bez_cal3=t_bez_cal*t_bez_cal*t_bez_cal
             t_bez_cal2=t_bez_cal*t_bez_cal
             
-            x_bez_cal = ax*(t_bez_cal3) + bx*(t_bez_cal2) + cx*t_bez_cal + x1
-            y_bez_cal = ay*(t_bez_cal3) + by*(t_bez_cal2) + cy*t_bez_cal + y1
+            x_bez_cal = ax*(t_bez_cal3) + bx*(t_bez_cal2) + cx*t_bez_cal + xI
+            y_bez_cal = ay*(t_bez_cal3) + by*(t_bez_cal2) + cy*t_bez_cal + yI
 
             Delta_x_imput_eff=fabs(x_imput-x_bez_cal)
+            
+        X_out=(x_imput+x_bez_cal)/2
+        Y_out=y_bez_cal
     
         if    t_bez_cal>1.001:            
             return None
         else: 
-            return x_imput,y_bez_cal
+            return X_out,Y_out
         
    
-    def calc_bez_t (self,t_bez_cal,x1,y1,x2,y2,x3,y3,x4,y4):
-        """(t_bez_cal,x1,y1,x2,y2,x3,y3,x4,y4)
+    def calc_bez_t (t_bez_cal,xI,yI,xII,yII,xIII,yIII,xIV,yIV):
+        """(t_bez_cal,xI,yI,xII,yII,xIII,yIII,xIV,yIV)
         calculate the x,y on a bezier for a given t 0<1
         return a tuple (x,y)
         """
-        if   t_bez_cal<0 or t_bez_cal>1.001:            
+    
+        if t_bez_cal<0 or t_bez_cal>1:
             return None
     
-
-    
-        cx = 3*(x2 - x1)
-        bx = 3*(x3 - x2) - cx
-        ax = x4 - x1 - cx - bx
-        cy = 3*(y2 - y1)
-        by = 3*(y3 - y2) - cy
-        ay = y4 - y1 - cy - by
+        cx = 3*(xII - xI)
+        bx = 3*(xIII - xII) - cx
+        ax = xIV - xI - cx - bx
+        cy = 3*(yII - yI)
+        by = 3*(yIII - yII) - cy
+        ay = yIV - yI - cy - by
     
         t_bez_cal3=t_bez_cal*t_bez_cal*t_bez_cal
         t_bez_cal2=t_bez_cal*t_bez_cal
-        x_bez_cal = ax*(t_bez_cal3) + bx*(t_bez_cal2) + cx*t_bez_cal + x1
-        y_bez_cal = ay*(t_bez_cal3) + by*(t_bez_cal2) + cy*t_bez_cal + y1
+
+        x_bez_cal = ax*(t_bez_cal3) + bx*(t_bez_cal2) + cx*t_bez_cal + xI
+        y_bez_cal = ay*(t_bez_cal3) + by*(t_bez_cal2) + cy*t_bez_cal + yI
 
         return x_bez_cal,y_bez_cal
-
-    def calc_int_bez (self,x1,y1,x2,y2,x3,y3,x4,y4,X1,Y1,X2,Y2,X3,Y3,X4,Y4):
-
-    
-        t=0
-        #start the calculation on the first curve
-    
-        calc_bez_xyl_1= self.calc_bez_length (x1,y1,x2,y2,x3,y3,x4,y4,0.1)
-        Delta_t_incremental=calc_bez_xyl_1[1]
-    
-        calc_bez_xyl_2= self.calc_bez_length (X1,Y1,X2,Y2,X3,Y3,X4,Y4,0.1)
-        Delta_t_bez_cal=calc_bez_xyl_2[1]
-    
-        coor= self.calc_bez_t (t,x1,y1,x2,y2,x3,y3,x4,y4)
-    
-        coor2= self.calc_bez_xy (coor[0],X1,Y1,X2,Y2,X3,Y3,X4,Y4,0.1, Delta_t_bez_cal)
-
-        delta_ins=hypot(coor[0]-coor2[0],coor[1]-coor2[1])
-        delta_ins_pre=delta_ins
-
-        delta_t_bez=delta_ins_pre/100000
-
-    
-        while True:
-            if t  > 1 or delta_ins > delta_ins_pre :
-                break
-            else:
-                pass
-            
-            if delta_t_bez>0.0005:
-                delta_t_bez=delta_ins_pre/100000
-            else:
-                delta_t_bez=0.0005
-            t_pre=t
-            t=t+delta_t_bez
-            delta_ins_pre=delta_ins
-            coor= self.calc_bez_t (t,x1,y1,x2,y2,x3,y3,x4,y4)
-    
-            coor2= self.calc_bez_xy (coor[0],X1,Y1,X2,Y2,X3,Y3,X4,Y4,0.1, Delta_t_bez_cal)
-            delta_ins=hypot(coor[0]-coor2[0],coor[1]-coor2[1])
-    
-    
-    
-        coor= self.calc_bez_t (t_pre,x1,y1,x2,y2,x3,y3,x4,y4)
-        X_out=coor[0]
-        Y_out=coor[1]
-    
-        if t > 1.01 or delta_ins >10 :
-            return None
-        else:
-            return X_out,Y_out
