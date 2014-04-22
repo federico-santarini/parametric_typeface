a54e3b2bf5b3b9f9275637ab320b887840d204b3
Giovanni P <pignoni.g@gmail.com>
2014-04-22 10:09:07 +0200

    accorpati i due cal bez xy

--- calc/bezIntersection.py
+++ calc/bezIntersection.py
@@ -54,91 +54,53 @@ class bezierIntersection:
         Delta_t_bez_cal= Delta_x_imput_max / l_bex            
         return l_bex, Delta_t_bez_cal
     
-	
+    
     def calc_bez_xy (XI,xI,yI,xII,yII,xIII,yIII,xIV,yIV,Delta_x_imput_max,Delta_t_bez_cal):
+        
         """(XI,xI,yI,xII,yII,xIII,yIII,xIV,yIV,Delta_x_imput_max,Delta_t_bez_cal)
         calculate y coordinate on a bezier curve given a x value
         Delta_x_imput_max is the precision required es (0.001) 
-        Delta_t_bez_cal is the scan precision es (0.001) """
-        x_imput=XI		
+        Delta_t_bez_cal is the scan precision es (0.001) 
+        if Delta_t_bez_cal is == a sutable number will be calculated using calc_bez_length """
+        
+        if Delta_t_bez_cal== None:
+            calc_bez= calc_bez_length (self,xI,yI,xII,yII,xIII,yIII,xIV,yIV,Delta_x_imput_max)
+            Delta_t_bez_cal=calc_bez[1]
+        x_imput=XI        
         t_bez_cal=0
-	
+    
         cx = 3*(xII - xI)
         bx = 3*(xIII - xII) - cx
         ax = xIV - xI - cx - bx
         cy = 3*(yII - yI)
         by = 3*(yIII - yII) - cy
         ay = yIV - yI - cy - by
-	
+    
         Delta_x_imput_eff=1000000000
 
         while True:
             if t_bez_cal>1 or Delta_x_imput_eff <= Delta_x_imput_max:
                 break
             else:
-                pass					
+                pass                    
             t_bez_cal=t_bez_cal+Delta_t_bez_cal    
             t_bez_cal3=t_bez_cal*t_bez_cal*t_bez_cal
             t_bez_cal2=t_bez_cal*t_bez_cal
-			
+            
             x_bez_cal = ax*(t_bez_cal3) + bx*(t_bez_cal2) + cx*t_bez_cal + xI
             y_bez_cal = ay*(t_bez_cal3) + by*(t_bez_cal2) + cy*t_bez_cal + yI
 
             Delta_x_imput_eff=fabs(x_imput-x_bez_cal)
-			
+            
         X_out=(x_imput+x_bez_cal)/2
         Y_out=y_bez_cal
-	
-        if	t_bez_cal>1.001:			
+    
+        if    t_bez_cal>1.001:            
             return None
         else: 
             return X_out,Y_out
-	    
-    def calc_bez_xy_auto (XI,xI,yI,xII,yII,xIII,yIII,xIV,yIV,Delta_x_imput_max):
-        """
-            (XI,xI,yI,xII,yII,xIII,yIII,xIV,yIV,Delta_x_imput_max)
-            calculate y coordinate on a bezier curve given a x value
-            Delta_x_imput_max is the precision required es (0.001) 
-             Delta_t_bez_cal is automatically calculated using calc_bez_length"""
-    
-        calc_bez= calc_bez_length (xI,yI,xII,yII,xIII,yIII,xIV,yIV,Delta_x_imput_max)
-        Delta_t_bez_cal=calc_bez[1]
-    
-        x_imput=XI
-        t_bez_cal=0
-	
-        cx = 3*(xII - xI)
-        bx = 3*(xIII - xII) - cx
-        ax = xIV - xI - cx - bx
-        cy = 3*(yII - yI)
-        by = 3*(yIII - yII) - cy
-        ay = yIV - yI - cy - by
-	
-        Delta_x_imput_eff=1000000000
-
-        while True:
-            if t_bez_cal>1 or Delta_x_imput_eff <= Delta_x_imput_max:
-                break
-            else:
-                pass					
-            t_bez_cal=t_bez_cal+Delta_t_bez_cal
-            t_bez_cal3=t_bez_cal*t_bez_cal*t_bez_cal
-            t_bez_cal2=t_bez_cal*t_bez_cal
-			
-            x_bez_cal = ax*(t_bez_cal3) + bx*(t_bez_cal2) + cx*t_bez_cal + xI
-            y_bez_cal = ay*(t_bez_cal3) + by*(t_bez_cal2) + cy*t_bez_cal + yI
-
-            Delta_x_imput_eff=fabs(x_imput-x_bez_cal)
-			
-        X_out=(x_imput+x_bez_cal)/2
-        Y_out=y_bez_cal
-	
-        if	t_bez_cal>1.001:			
-            return None
-        else: 
-            return X_out,Y_out
-
-
+        
+   
     def calc_bez_t (t_bez_cal,xI,yI,xII,yII,xIII,yIII,xIV,yIV):
         """(t_bez_cal,xI,yI,xII,yII,xIII,yIII,xIV,yIV)
         calculate the x,y on a bezier for a given t 0<1
@@ -147,14 +109,14 @@ class bezierIntersection:
     
         if t_bez_cal<0 or t_bez_cal>1:
             return None
-	
+    
         cx = 3*(xII - xI)
         bx = 3*(xIII - xII) - cx
         ax = xIV - xI - cx - bx
         cy = 3*(yII - yI)
         by = 3*(yIII - yII) - cy
         ay = yIV - yI - cy - by
-	
+    
         t_bez_cal3=t_bez_cal*t_bez_cal*t_bez_cal
         t_bez_cal2=t_bez_cal*t_bez_cal
 
