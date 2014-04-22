260f9dfb782af1b049b4ea601f01d339429a1792
Giovanni P <pignoni.g@gmail.com>
2014-04-22 16:15:27 +0200

    correzioni a sandbox

--- sandbox giovanni.py
+++ sandbox giovanni.py
@@ -6,15 +6,16 @@
 from calc import bezIntersection
 
 
-bezierIntersection= bezIntersection.bezierIntersection()
-length= bezierIntersection.calc_bez_length(10,10,1,1,1,1,100,100,1)
+bezTool= bezIntersection.bezierIntersection()
+
+length= bezTool.calc_bez_length(10,10,1,1,1,1,100,100,1)
 
 print"length",length
 
-xy= bezierIntersection.calc_bez_xy(11,10,10,1,1,1,1,100,100,1,None)
+xy= bezTool.calc_bez_xy(11,10,10,1,1,1,1,100,100,1,None)
 
 print"xy",xy
 
-t= bezierIntersection.calc_bez_t(0.5,10,10,1,1,1,100,100,1)
+t= bezTool.calc_bez_t(0.5,10,10,1,1,1,100,100,1)
 
 print"t",t
