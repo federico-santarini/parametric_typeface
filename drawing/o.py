f12c8c047ff91b4574bdbedab22e64b532fdc272
Giovanni P <pignoni.g@gmail.com>
2014-04-29 16:49:43 +0200

    Draw n and Draw o
    working on Draw n and Draw o

--- drawing/o.py
+++ drawing/o.py
@@ -2,7 +2,120 @@
 # o #
 #####
 
+import robofab.world
+import math
+from robofab.world import *
+from math import *
+from shapes import quarter
+
 class lowercase_o():
     
     def __init__(self):
         pass
+    
+    def drawing(self):
+        
+        ## Testing quarter class
+        
+        q = quarter.Quarter()
+        
+        # Chiamata del glifo
+        font = CurrentFont ()
+        glyph = font ['o']
+        glyph.clear()
+
+        ### upperLeft
+        # Punti di ancoraggio
+        x1 = 3 
+        y1 = 238
+        x2 = 138 
+        y2 = 240
+        x3 = 225 
+        y3 = 368
+        x4 = 230 
+        y4 = 462
+
+        # Manipolatori
+        hnd_int_ver = 64
+        hnd_int_hor = 62
+        hnd_ext_ver = 137
+        hnd_ext_hor = 137
+
+        # Disegno
+        q.upperLeft(glyph, 
+                  x1, y1, x2, y2, x3, y3, x4, y4, 
+                  hnd_int_ver, hnd_int_hor, 
+                  hnd_ext_ver, hnd_ext_hor)
+          
+
+        ### upperRight
+        # Punti di ancoraggio
+        x1 = 328 
+        y1 = 239
+        x2 = 465 
+        y2 = 243
+        x3 = 249 
+        y3 = 461
+        x4 = 243 
+        y4 = 366
+
+        # Manipolatori
+        hnd_int_ver = 63
+        hnd_int_hor = 60
+        hnd_ext_ver = 128
+        hnd_ext_hor = 129
+
+        # Disegno
+        q.upperRight(glyph, 
+                  x1, y1, x2, y2, x3, y3, x4, y4,
+                  hnd_int_ver, hnd_int_hor, 
+                  hnd_ext_ver, hnd_ext_hor)
+
+        ### bottomLeft
+        # Punti di ancoraggio
+        x1 = 222
+        y1 = 0
+        x2 = 224
+        y2 = 94
+        x3 = 138
+        y3 = 221
+        x4 = 3
+        y4 = 218
+
+        # Manipolatori
+        hnd_int_ver = 72
+        hnd_int_hor = 56
+        hnd_ext_ver = 137
+        hnd_ext_hor = 124
+
+        # Disegno
+        q.bottomLeft(glyph,
+                  x1, y1, x2, y2, x3, y3, x4, y4,
+                  hnd_int_ver, hnd_int_hor, 
+                  hnd_ext_ver, hnd_ext_hor)
+          
+          
+        ### bottomRight
+        # Punti di ancoraggio
+        x1 = 243
+        y1 = 0
+        x2 = 466
+        y2 = 225
+        x3 = 329
+        y3 = 222
+        x4 = 245
+        y4 = 94
+
+        # Manipolatori
+        hnd_int_ver = 76
+        hnd_int_hor = 52
+        hnd_ext_ver = 154
+        hnd_ext_hor = 113
+
+        # Disegno
+        q.bottomRight(glyph,
+                  x1, y1, x2, y2, x3, y3, x4, y4,
+                  hnd_int_ver, hnd_int_hor, 
+                  hnd_ext_ver, hnd_ext_hor)
+
+        
--- sandbox giovanni.py
+++ sandbox giovanni.py
@@ -18,35 +18,10 @@ print"xy",xy
 
 t= bezTool.calc_bez_t(0.5,((10,10),(1,1),(1,100),(100,1)))
 
-print"t",t
-#inserimento dati
-XI=583
-YI=416
-XII=818
-YII=416
-XIII=861
-YIII=304
-XIV=861
-YIV=83
-xI=850
-yI=450
-xII=750
-yII=450
-xIII=650
-yIII=380
-xIV=650
-yIV=62
 
-inter=bezTool.calc_int_bez ( ((xI,yI),(xII,yII),(xIII,yIII),(xIV,yIV)) , ((XI,YI),(XII,YII),(XIII,YIII),(XIV,YIV)) )
-##################################################
-font = CurrentFont ()
-glyph = font ['c']
-glyph.clear ()
+oDraw = lowercase_o.drawing()
+nDraw = lowercase_n.drawing()
 
-pen = glyph.getPen ()
-pen.moveTo ((XI,YI))
-pen.curveTo ((XII,YII),(XIII,YIII),(XIV,YIV))
-pen.closePath()
 
 pen.moveTo ((xI,yI))
 pen.curveTo ((xII,yII),(xIII,yIII),(xIV,yIV))
