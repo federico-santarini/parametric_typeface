633696673cf261823d9355a1c4d7b5e64f70e49a
roberto-arista <arista.rob@gmail.com>
2014-04-29 17:58:56 +0200

    First Polygon draft

--- shapes/polygon.py
+++ shapes/polygon.py
@@ -1,3 +1,21 @@
 ###########
 # Polygon #
 ###########
+
+class Polygon:
+
+    def __init__(self):
+        pass
+        
+    def drawPolyByPoints(self, glyph, points_list):
+        pen = glyph.getPen()
+    
+        # Starting point
+        pen.moveTo(points_list[0])
+    
+        # Extremes    
+        for point_coordinates in points_list[1:]:
+            pen.lineTo(point_coordinates)
+    
+        # Closing path
+        pen.closePath()
