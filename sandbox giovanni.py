cdb71395ce8c78e47f586dd60bb16c8ee78b1233
Giovanni P <pignoni.g@gmail.com>
2014-04-29 16:49:59 +0200

    testing Draw n and Draw o

--- sandbox giovanni.py
+++ sandbox giovanni.py
@@ -3,30 +3,32 @@
 ###################
 
 # Importing modules
+import robofab.world
+import math
+from robofab.world import *
+from math import *
+
 from calc import bezIntersection
+from shapes import quarter
+from drawing import a, b, c, d, e, i, l, m, n, o, p, q, u
 
-
-bezTool= bezIntersection.bezierIntersection()
-
-length= bezTool.calc_bez_length( ((10,10),(1,1),(1,1),(100,100)),1)
-
-print"length",length
-
-xy= bezTool.calc_bez_xy(11,((10,10),(1,1),(1,1),(100,100)),1,None)
-
-print"xy",xy
-
-t= bezTool.calc_bez_t(0.5,((10,10),(1,1),(1,100),(100,1)))
+lowercase_a = a.lowercase_a()
+lowercase_b = b.lowercase_b()
+lowercase_c = c.lowercase_c()
+lowercase_d = d.lowercase_d()
+lowercase_e = e.lowercase_e()
+lowercase_i = i.lowercase_i()
+lowercase_l = l.lowercase_l()
+lowercase_m = m.lowercase_m()
+lowercase_n = n.lowercase_n()
+lowercase_o = o.lowercase_o()
+lowercase_p = p.lowercase_p()
+lowercase_q = q.lowercase_q()
+lowercase_u = u.lowercase_u()
 
 
 oDraw = lowercase_o.drawing()
 nDraw = lowercase_n.drawing()
 
 
-pen.moveTo ((xI,yI))
-pen.curveTo ((xII,yII),(xIII,yIII),(xIV,yIV))
-pen.closePath()
 
-pen.moveTo ((0,0))
-pen.lineTo ((inter))
-pen.closePath()
